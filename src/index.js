413b3d84cb7460f006436517a76f6e4b5117a312
aarpitgoel <arpitgoelcsd@gmail.com>
2020-12-13 00:45:44 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ app.use(express.json());
 const { connection } = require('./connector')
 const data=require('./data');
 
+let recoveredPatients = 0;
+for(let i=0 ; i<data.length ; i++){
+    recoveredPatients +=data[i].recovered;
+}
+
 app.get('/totalRecovered',(req,res)=>{   
-    let obj={data:{_id: "total", recovered:135481}};
+    let obj={data:{_id: "total", recovered:recoveredPatients}};
     res.send(obj);
 })
 
